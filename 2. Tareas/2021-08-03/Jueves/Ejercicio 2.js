27cb503a20ceee6884eda6b63900b12c56376806
Enrique Palomino <octepc.97@gmail.com>
2021-08-26 17:36:45 -0500

    Ejercicio 2 terminado

--- 2. Tareas/2021-08-03/Jueves/Ejercicio 2.js
+++ 2. Tareas/2021-08-03/Jueves/Ejercicio 2.js
@@ -11,3 +11,28 @@
 // It supposed to be filled using it's own function
 
 // Add any type of element on the object.. arrays, objects, int, strings, etc
+
+
+let myObject1 = {
+  myFunction: function(prop, _value){
+    Object.defineProperty(this, prop,{
+      value: _value,
+      enumerable: true
+    })
+  }
+}
+
+myObject1.myFunction('name', 'Enrique');
+myObject1.myFunction('lastName', 'Palomino');
+myObject1.myFunction('age', 24);
+myObject1.myFunction('pets', false);
+
+console.log(myObject1)
+
+// Freeze impide la sobreescritura del código. En este caso es especialmente usado porque tanto los objetos como los arrays pueden ser modificados en el sentido de añadir propiedades o valores a pesar de que la función sea una constante.
+
+Object.freeze(myObject1);
+
+myObject1.myFunction('skills', ['swimng', 'ride bycicles'])
+
+console.log(myObject1) //efectivamente, la consola devuelve un error: Cannot define property skills, object is not extensible
